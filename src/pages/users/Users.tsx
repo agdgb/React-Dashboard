d05067058a8713c2f06b27b28b3e79c9aaa77373
amanuel <137877737+agdgb@users.noreply.github.com>
2024-06-19 11:53:45 +0300

    mui data grid added

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import "./users.scss";
+import DataTable from "../../components/AreaChartBox/DataTable/DataTable";
+import { GridColDef } from "@mui/x-data-grid";
+import { userRows } from "../../data";
+
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Avatar",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "firstName",
+    type: "string",
+    headerName: "First name",
+    width: 150,
+  },
+  {
+    field: "lastName",
+    type: "string",
+    headerName: "Last name",
+    width: 150,
+  },
+  {
+    field: "email",
+    type: "string",
+    headerName: "Email",
+    width: 200,
+  },
+  {
+    field: "phone",
+    type: "string",
+    headerName: "Phone",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "verified",
+    headerName: "Verified",
+    width: 150,
+    type: "boolean",
+  },
+];
 
 function Users() {
   return (
-    <div>Users</div>
-  )
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+
+      <div className="gridcontainer">
+        <DataTable slug="users" columns={columns} rows={userRows} />
+      </div>
+    </div>
+  );
 }
 
-export default Users
+export default Users;
