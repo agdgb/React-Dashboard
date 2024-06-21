9bfaf989d85ab94d2151d80456e3cdcdd834edb5
amanuel <137877737+agdgb@users.noreply.github.com>
2024-06-21 15:52:48 +0300

    popup, users & products done with reusable mui data grid

---
+++ src/components/PopupBox/Add.tsx
@@ -0,0 +1,43 @@
+import { GridColDef } from "@mui/x-data-grid";
+import React from "react";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const Add = (props: Props) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    alert("form submitted!");
+  };
+
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => props.setOpen(false)}>
+          X
+        </span>
+        <h1>Add new {props.slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {props.columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          {/* <div className="buttons"> */}
+          <button>Submit</button>
+          {/* <button>Cancel</button> */}
+          {/* </div> */}
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
